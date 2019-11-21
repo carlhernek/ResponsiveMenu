cd452910f0c188c72af1bede48e887f584cec2f6
carlhernek <53989735+carlhernek@users.noreply.github.com>
2019-11-21 19:19:03 +0100

    Some final touches

--- js/script.js
+++ js/script.js
@@ -1,10 +1,14 @@
 let hmbgMenu = document.getElementById("hamburger");
+let drpdMenu = document.getElementById("dropdown");
 let halfScreen = Math.floor(window.innerWidth/2);
 
 function touchCoordinates(event) {
     let x = event.touches[0].clientX;
     // let y = event.touches[0].clientY;
     if (x < halfScreen) { 
+        drpdMenu.classList.remove("dropdown-center");
+        drpdMenu.classList.remove("dropdown-right");
+        drpdMenu.classList.add("dropdown-left");
         hmbgMenu.classList.remove("hamburger-center");
         hmbgMenu.classList.remove("hamburger-right");
         hmbgMenu.classList.add("hamburger-left");
@@ -13,6 +17,28 @@ function touchCoordinates(event) {
         hmbgMenu.classList.remove("hamburger-center");
         hmbgMenu.classList.remove("hamburger-left");
         hmbgMenu.classList.add("hamburger-right");
+        drpdMenu.classList.remove("dropdown-center");
+        drpdMenu.classList.remove("dropdown-left");
+        drpdMenu.classList.add("dropdown-right");
     };
 };
 
+let eOne = 1;
+
+hmbgMenu.onclick = function() {
+
+    switch(eOne) {
+
+    case 1:
+        drpdMenu.style.opacity = "1";
+        drpdMenu.style.height = "35vh";
+        eOne = 2
+        break;
+    
+    case 2:
+        drpdMenu.style.opacity = "0";
+        drpdMenu.style.height = "0vh";
+        eOne = 1
+        break;
+    }
+}
