8a8bf17334c5d7fbc64e34ecc7ab21699291e9bd
carlhernek <53989735+carlhernek@users.noreply.github.com>
2019-11-21 17:30:05 +0100

    Cleaned

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
+let hmbgMenu = document.getElementById("hamburger");
 let halfScreen = Math.floor(window.innerWidth/2);
-let meme = document.getElementById("meme");
-
-
-console.log(meme);
 
 function touchCoordinates(event) {
     let x = event.touches[0].clientX;
-    let y = event.touches[0].clientY;
-    meme.innerHTML = x + ", " + y;
-}
+    // let y = event.touches[0].clientY;
+    if (x < halfScreen) { 
+        hmbgMenu.style.alignSelf = "flex-start";
+    };
+    if (x > halfScreen) { 
+        hmbgMenu.style.alignSelf = "flex-end" 
+    };
+};
